ed7cf375a1f71b8b084f8006bbb6e7bfcb2d464b
John A Stevenson <jostev@bgs.ac.uk>
2025-11-03 18:58:47 +0000

    Refactor to trees example with foreign keys

--- pygeodiff/tests/test_geodiff_rebase_fk.py
+++ pygeodiff/tests/test_geodiff_rebase_fk.py
@@ -1,3 +1,16 @@
+"""
+This file tests the behaviour of geodiff when there are database-level constraints
+applied to the tables.
+
+Some of the tests are expected to fail until issue 210 has been resolved.
+https://github.com/MerginMaps/geodiff/issues/210
+
+The behaviour of GeoDiff in scenarios where combining changes from two users will
+cause a database constraint to fail is as-yet undefined.  Perhaps it should raise
+a GeoDiffConstraintError that reports the error message from the database with
+information about the constraint that was broken.
+"""
+
 import os
 from pathlib import Path
 import re
@@ -13,23 +26,23 @@ GEODIFFLIB = os.environ.get("GEODIFFLIB", None)
 
 
 @pytest.mark.parametrize(
-    "fk_constrained",
+    "db_constrained",
     [
-        pytest.param(False, id="no_fk_constraint"),
+        pytest.param(False, id="no_db_constraint"),
         pytest.param(
             True,
             marks=pytest.mark.xfail(
                 raises=GeoDiffLibError,
                 reason="Expected to fail due to issue 210, when this xpasses remove this decorator",
             ),
-            id="fkconstraint",
+            id="dbconstraint",
         ),
     ],
 )
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
-def test_geodiff_rebase_fk_happy_path(fk_constrained, user_a_data_first, tmp_path):
+def test_geodiff_rebase_fk_happy_path(db_constrained, user_a_data_first, tmp_path):
     """
     This test checks that rebase succeeds on simple changes on tables with
     foreign key constraints.  This test applies INSERT, UPDATE and DELETE
@@ -38,47 +51,79 @@ def test_geodiff_rebase_fk_happy_path(fk_constrained, user_a_data_first, tmp_pat
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_gpkg_files(tmp_path, fk_constrained=False)
+    original, user_a, user_b = create_db_files(tmp_path, db_constrained=db_constrained)
 
     # Apply changes to databases to give the following expected values
-    expected_parents = set(['p1_updated', 'p2', 'p3', 'p4'])  # One edited, two added
-    expected_children = set(['p1_child2', 'p2_child1', 'p2_child2', 'p3_child1'])  # One added, one deleted
+    expected = {
+        "species": [
+            {"species_id": "MPL", "name": "Maple"},
+            {"species_id": "OAK", "name": "Oak"},
+            # 'PIN' deleted
+            {"species_id": "SPC", "name": "Spruce"},  # inserted
+            {"species_id": "BCH", "name": "Birch"},  # inserted
+        ],
+        "trees": [
+            {"tree_id": 20251103001, "species_id": "MPL", "age": 1},  # age updated
+            {"tree_id": 20251103002, "species_id": "OAK", "age": 99},  # age updated
+            # 20241103003 deleted
+            {"tree_id": 20251103004, "species_id": "SPC", "age": 29},  # inserted
+            {"tree_id": 20251103005, "species_id": "BCH", "age": 46},  # inserted
+        ]
+    }
 
-    with sqlite3.connect(user_a) as conn_a, sqlite3.connect(user_b) as conn_b:
+    with sqlite3.connect(user_a) as conn_a:
         conn_a.execute(
             """
-            INSERT INTO parent VALUES (
-                null,
-                '6545d3eb-1cbd-45c6-9121-6758a6749866',
-                'p3'
-            )"""
+            INSERT INTO species (species_id, name) VALUES (
+                'BCH', 'Birch'
+            )
+            """
         )
         conn_a.execute(
             """
-            INSERT INTO child VALUES (
-                null,
-                '7d3cc6fc-f8f0-46b9-9f58-e6b9029a632e',
-                '6545d3eb-1cbd-45c6-9121-6758a6749866',
-                'p3_child1'
-            )"""
+            INSERT INTO trees (tree_id, species_id, age) VALUES (
+                20251103005, 'BCH', 46
+            )
+            """
         )
         conn_a.execute(
             """
-            UPDATE parent SET name='p1_updated' WHERE name='p1'
+            DELETE FROM trees WHERE tree_id IS 20251103003
+            """
+        )
+        conn_a.execute(
+            """
+            DELETE FROM species WHERE species_id IS 'PIN'
+            """
+        )
+        conn_a.execute(
+            """
+            UPDATE trees SET age=1 WHERE tree_id=20251103001
+            """
+        )
+
+    with sqlite3.connect(user_b) as conn_b:
+        conn_b.execute(
+            """
+            INSERT INTO species (species_id, name) VALUES (
+                'SPC', 'Spruce'
+            )
             """
         )
         conn_b.execute(
             """
-            INSERT INTO parent VALUES (
-                null,
-                'bec8d2d1-454e-41ef-83cc-900fac406f7f',
-                'p4'
+            INSERT INTO trees (tree_id, species_id, age) VALUES (
+                20251103004, 'SPC', 29
             )
             """
         )
-        conn_b.execute("DELETE FROM child WHERE name='p1_child1'")
+        conn_b.execute(
+            """
+            UPDATE trees SET age=99 WHERE tree_id=20251103002
+            """
+        )
 
-    # Set the argument order, i.e. which gpkg should be the rebased result
+    # Set the argument order, i.e. which db should be the rebased result
     if user_a_data_first:
         theirs, mine = user_a, user_b
     else:
@@ -88,98 +133,90 @@ def test_geodiff_rebase_fk_happy_path(fk_constrained, user_a_data_first, tmp_pat
     geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
 
     # Assert that rebased database contains expected changes
-    assert_names("parent", expected_parents, mine)
-    assert_names("child", expected_children, mine)
+    assert_data_as_expected(mine, expected)
 
 
-def assert_names(table: str, expected_names: set[str], db: Path) -> bool:
+def assert_data_as_expected(db: Path, expected_data: dict[str, list[dict]]):
+    """
+    Assert that table contents are as expected.  Use `set` comparison because
+    the row ordering depends on the order that tables are passed to `rebase`.
+    """
     with sqlite3.connect(db) as conn:
-        names = set(
-            row[0] for row in
-            conn.execute(f"SELECT name FROM {table}").fetchall()
-        )
-    assert names == expected_names
+        for table, rows in expected_data.items():
+            column_names = rows[0].keys()
+            results = conn.execute(
+                f"""SELECT {", ".join(column_names)} FROM {table}"""
+                ).fetchall()
+
+            assert set(results) == set(tuple(row.values()) for row in rows)
 
 
-def create_gpkg_files(tmp_path: Path, fk_constrained: bool = True) -> Tuple[Path, Path, Path]:
+def create_db_files(tmp_path: Path, db_constrained: bool = True) -> Tuple[Path, Path, Path]:
     """
-    Create 3 GeoPackage files at the given filepaths, create the
-    same tables with the same original data: two parents, each with
-    two children.
+    Create 3 SQLite files at the given filepaths, each with the same tables
+    and data.
+
+    If `db_constrained`, the database will apply UNIQUE and FOREIGN
+    KEY constraints.
     """
-    original = tmp_path / "original.gpkg"
-    user_a = tmp_path / "user_a.gpkg"
-    user_b = tmp_path / "user_b.gpkg"
+    original = tmp_path / "original.db"
+    user_a = tmp_path / "user_a.db"
+    user_b = tmp_path / "user_b.db"
 
     # Define tables
-    create_parent_sql = """
-        CREATE TABLE parent (
+    create_species_sql = """
+        CREATE TABLE species (
             fid INTEGER PRIMARY KEY,
-            uuid TEXT UNIQUE NOT NULL,
+            species_id TEXT UNIQUE,
             name TEXT
         )"""
-    create_child_sql = """
-        CREATE TABLE child (
+    create_trees_sql = """
+        CREATE TABLE trees (
             fid INTEGER PRIMARY KEY,
-            uuid TEXT UNIQUE NOT NULL,
-            parent_uuid TEXT NOT NULL,
-            name TEXT,
-            FOREIGN KEY("parent_uuid") REFERENCES "parent"("uuid")
+            tree_id INTEGER UNIQUE NOT NULL,
+            species_id TEXT NOT NULL,
+            age INTEGER,
+            FOREIGN KEY("species_id") REFERENCES "species"("species_id")
         )"""
 
-    if not fk_constrained:
+    if not db_constrained:
         # Remove database constraints from table definitions
-        create_parent_sql = re.sub(r" UNIQUE", "", create_parent_sql)
-        create_child_sql = re.sub(r" UNIQUE", "", create_child_sql)
-        create_child_sql = re.sub(r",.*FOREIGN.*REFERENCES.*\)", "", create_child_sql)
+        create_species_sql = re.sub(r" UNIQUE", "", create_species_sql)
+        create_trees_sql = re.sub(r" UNIQUE", "", create_trees_sql)
+        create_trees_sql = re.sub(r",.*FOREIGN.*REFERENCES.*\)", "", create_trees_sql)
 
-    # Define test data
-    parent_data = [
-        {"uuid": "8b0cbf9c-cb22-4b7a-a4cf-4a4c31f48732", "name": "p1"},
-        {"uuid": "3dcae64d-afe0-4233-b771-437b7ca9ed99", "name": "p2"},
-    ]
-    child_data = [
-        {
-            "uuid": "3544b178-2521-4b7c-a33e-1b10e8c6264e",
-            "parent_uuid": "8b0cbf9c-cb22-4b7a-a4cf-4a4c31f48732",
-            "name": "p1_child1",
-        },
-        {
-            "uuid": "b64f3590-be92-4dad-b30f-a450c94846b8",
-            "parent_uuid": "8b0cbf9c-cb22-4b7a-a4cf-4a4c31f48732",
-            "name": "p1_child2",
-        },
-        {
-            "uuid": "761b633a-d31d-4ea4-96e8-247aef434bc8",
-            "parent_uuid": "3dcae64d-afe0-4233-b771-437b7ca9ed99",
-            "name": "p2_child1",
-        },
-        {
-            "uuid": "54a6feba-4297-400b-bf8c-058cd16b8572",
-            "parent_uuid": "3dcae64d-afe0-4233-b771-437b7ca9ed99",
-            "name": "p2_child2",
-        },
-    ]
+    # Define initial data
+    species_data = [
+            {"species_id": "MPL", "name": "Maple"},
+            {"species_id": "OAK", "name": "Oak"},
+            {"species_id": "PIN", "name": "Pine"},
+        ]
+    trees_data = [
+            {"tree_id": 20251103001, "species_id": "MPL", "age": 25},
+            {"tree_id": 20251103002, "species_id": "OAK", "age": 30},
+            {"tree_id": 20251103003, "species_id": "PIN", "age": 18},
+        ]
 
     # Create geopackages
-    for gpkg in [original, user_a, user_b]:
-        with sqlite3.connect(gpkg) as conn:
-            conn.execute(create_parent_sql)
-            conn.execute(create_child_sql)
+    with sqlite3.connect(original) as conn:
+        conn.execute(create_species_sql)
+        conn.execute(create_trees_sql)
+        conn.executemany(
+            """
+            INSERT INTO species (species_id, name)
+            VALUES (:species_id, :name)
+            """,
+            species_data
+        )
+        conn.executemany(
+            """
+            INSERT INTO trees (tree_id, species_id, age)
+            VALUES (:tree_id, :species_id, :age)
+            """,
+            trees_data
+        )
 
-            conn.executemany(
-                """
-                INSERT INTO parent (uuid, name)
-                VALUES (:uuid, :name)
-                """,
-                (row for row in parent_data)
-            )
-            conn.executemany(
-                """
-                INSERT INTO child (uuid, parent_uuid, name)
-                VALUES (:uuid, :parent_uuid, :name)
-                """,
-                (row for row in child_data)
-            )
+    user_a.write_bytes(original.read_bytes())
+    user_b.write_bytes(original.read_bytes())
 
     return original, user_a, user_b
